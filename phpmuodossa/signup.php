c5e7f0c877812993490b56e6047cc51417de25fc
PhantomLegioner <62619661+PhantomLegioner@users.noreply.github.com>
2020-04-01 14:11:54 +0300

    Add files via upload
    Korjattu puhelinnumeron teko tietokantaan ja sisäänkirjautumissession näkyminen.

--- phpmuodossa/signup.php
+++ phpmuodossa/signup.php
@@ -11,7 +11,7 @@
       <input type="text" name="sukunimi" placeholder="Sukunimi">
       <input type="text" name="paikkakunta" placeholder="Paikkakunta">
       <input type="text" name="osoite" placeholder="Osoite">
-      <input type="number" name="puhelinnumero" placeholder="Puhelinnumero">
+      <input type="text" name="puhelinnumero" placeholder="Puhelinnumero">
       <input type="text" name="sähköposti" placeholder="Sähköposti">
       <input type="password" name="salasana" placeholder="Salasana">
       <input type="password" name="salasana-toisto" placeholder="Kirjoita salasana uudestaan">
