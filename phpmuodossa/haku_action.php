4e43723d60c03f5ba45cc4b35c1ef0860e3089a0
jarnovanuef <62952859+jarnovanuef@users.noreply.github.com>
2020-04-01 11:51:25 +0300

    Add files via upload
    uus

--- phpmuodossa/haku_action.php
+++ phpmuodossa/haku_action.php
@@ -1,6 +1,6 @@
 <?php
 
-require 'includes/dbh.inc.php';
+require "includes/dbh.inc.php";
 
 ?>
 
@@ -22,41 +22,41 @@ $i = 0; //laskuri
 if(isset($_GET['nimi'])){
 	$qnimi = $_GET['nimi'];
 	if($i==0){
-		$query = $query + " WHERE " + "(`rt.ETUNIMI` LIKE '%".$qnimi."%') OR (`rt.SUKUNIMI` LIKE '%".$qnimi."%')";
+		$query = $query . " WHERE " . "(`rt.ETUNIMI` LIKE '%".$qnimi."%') OR (`rt.SUKUNIMI` LIKE '%".$qnimi."%')";
 		$i++;
 	}
 	else{
-		$query = $query + " AND " + "(`rt.ETUNIMI` LIKE '%".$qnimi."%') OR (`rt.SUKUNIMI` LIKE '%".$qnimi."%')";
+		$query = $query . " AND " . "(`rt.ETUNIMI` LIKE '%".$qnimi."%') OR (`rt.SUKUNIMI` LIKE '%".$qnimi."%')";
 	}
 }
 if(isset($_GET['paikkakunta'])){
 	$qpaikkakunta = $_GET['paikkakunta'];
 	if($i==0){
-		$query = $query + " WHERE " + "(`rt.PAIKKAKUNTA`='".$qpaikkakunta."')";
+		$query = $query . " WHERE " . "(`rt.PAIKKAKUNTA`='".$qpaikkakunta."')";
 		$i++;
 	}
 	else{
-		$query = $query + " AND " + "(`rt.PAIKKAKUNTA`='".$qpaikkakunta."')";
+		$query = $query . " AND " . "(`rt.PAIKKAKUNTA`='".$qpaikkakunta."')";
 	}
 }
 if(isset($_GET['cb_diabetes'])){
 	$qcb_diabetes = $_GET['cb_diabetes'];
 	if($i==0){
-		$query = $query + " WHERE " + "(`eo.OSAAMISEN_NIMI`='".$qcb_diabetes."')";
+		$query = $query . " WHERE " . "(`eo.OSAAMISEN_NIMI`='".$qcb_diabetes."')";
 		$i++;
 	}
 	else{
-		$query = $query + " AND " + "(`eo.OSAAMISEN_NIMI`='".$qcb_diabetes."')";
+		$query = $query . " AND " . "(`eo.OSAAMISEN_NIMI`='".$qcb_diabetes."')";
 	}
 }
 if(isset($_GET['cb_keliakia'])){
 	$qcb_keliakia = $_GET['cb_keliakia'];
 	if($i==0){
-		$query = $query + " WHERE " + "(`eo.OSAAMISEN_NIMI`='".$qcb_keliakia."')";
+		$query = $query . " WHERE " . "(`eo.OSAAMISEN_NIMI`='".$qcb_keliakia."')";
 		$i++;
 	}
 	else{
-		$query = $query + " AND " + "(`eo.OSAAMISEN_NIMI`='".$qcb_keliakia."')";
+		$query = $query . " AND " . "(`eo.OSAAMISEN_NIMI`='".$qcb_keliakia."')";
 	}
 }
 
@@ -64,7 +64,7 @@ $query = htmlspecialchars($query); //muuntaa html merkit sopiviksi
 
 $query = mysqli_real_escape_string($query); //estää sql-injektion
 
-$rawresults = mysql_query($query);
+$rawresults = mysqli_query($query);
 
 if(mysqli_num_rows($rawresults) > 0){ //jos tuloksia on
 	while($results = mysqli_fetch_array($rawresults)){ //looppaa kun tietojen siirto tauluun onnistuu
