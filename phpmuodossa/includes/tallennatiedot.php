ddc1d7b46d92daceffdec211fd57ca19db565b22
K0rppi <62621125+K0rppi@users.noreply.github.com>
2020-04-08 12:25:52 +0300

    Update tallennatiedot.php

--- phpmuodossa/includes/tallennatiedot.php
+++ phpmuodossa/includes/tallennatiedot.php
@@ -1,7 +1,9 @@
 <?php
-if(isset($_POST['tallenna_submit'])){
 	session_start();
 	require "dbh.inc.php";
+	$email = $_POST['email'];
+if(isset($_POST['tallenna_submit'])){
+	
 	$etunimi = $_POST['etunimi'];
 	$sukunimi = $_POST['sukunimi'];
 	$kaupunki = $_POST['paikkakunta'];
@@ -71,5 +73,72 @@ if(isset($_POST['tallenna_submit'])){
 	header("Location: ../profiili.php?change=succesful");
 	
 	
+}	
+
+else if(isset($_POST['btn_etunimi'])){
+	$sql = "UPDATE ravitsemusterapeutti SET ETUNIMI='' WHERE EMAIL='".$email."'";
+	$stmt = mysqli_stmt_init($conn);
+	if(!mysqli_stmt_prepare($stmt,$sql)){
+		header("Location: ../profiili.php?error=delete1");
+		exit();
+	}else{
+		
+		mysqli_stmt_execute($stmt);
+		$_SESSION['etunimi'] = "";
+	}
+	
+	header("Location: ../profiili.php?delete=success");
+}else if(isset($_POST['btn_sukunimi'])){
+	$sql = "UPDATE ravitsemusterapeutti SET SUKUNIMI='' WHERE EMAIL='".$email."'";
+	$stmt = mysqli_stmt_init($conn);
+	if(!mysqli_stmt_prepare($stmt,$sql)){
+		header("Location: ../profiili.php?error=delete2");
+		exit();
+	}else{
+		
+		mysqli_stmt_execute($stmt);
+		$_SESSION['sukunimi'] = "";
+	}
+	header("Location: ../profiili.php?delete=success");
+	
+	
+}else if(isset($_POST['btn_paikkakunta'])){
+	$sql = "UPDATE ravitsemusterapeutti SET PAIKKAKUNTA='' WHERE EMAIL='".$email."'";
+	$stmt = mysqli_stmt_init($conn);
+	if(!mysqli_stmt_prepare($stmt,$sql)){
+		header("Location: ../profiili.php?error=delete3");
+		exit();
+	}else{
+		
+		mysqli_stmt_execute($stmt);
+		$_SESSION['paikkakunta'] = "";
+	}
+	header("Location: ../profiili.php?delete=success");
+	
+}else if(isset($_POST['btn_osoite'])){
+	$sql = "UPDATE ravitsemusterapeutti SET OSOITE='' WHERE EMAIL='".$email."'";
+	$stmt = mysqli_stmt_init($conn);
+	if(!mysqli_stmt_prepare($stmt,$sql)){
+		header("Location: ../profiili.php?error=delete4");
+		exit();
+	}else{
+		
+		mysqli_stmt_execute($stmt);
+		$_SESSION['osoite'] = "";
+	}
+	header("Location: ../profiili.php?delete=success");
+	
+}else if(isset($_POST['btn_puhelin'])){
+	$sql = "UPDATE ravitsemusterapeutti SET PUHELINNRO='' WHERE EMAIL='".$email."'";
+	$stmt = mysqli_stmt_init($conn);
+	if(!mysqli_stmt_prepare($stmt,$sql)){
+		header("Location: ../profiili.php?error=delete5");
+		exit();
+	}else{
+		
+		mysqli_stmt_execute($stmt);
+		$_SESSION['puhelin'] = "";
+	}
+	header("Location: ../profiili.php?delete=success");
 	
 }
