930091c629fcb9a8c5e2e8e9de1270bc2626e894
jarnovanuef <62952859+jarnovanuef@users.noreply.github.com>
2020-04-11 12:22:39 +0300

    toiminnallisesti valmis

--- phpmuodossa/haku_action.php
+++ phpmuodossa/haku_action.php
@@ -10,24 +10,23 @@ require "includes/dbh.inc.php";
 
 <main>
 
-<button type="button" class="btn" style="background-color:#C1727B"><a href="haku.php">UUSI HAKU</button><br><br>
-
 <?php
 
-$query = "SELECT * FROM ravitsemusterapeutti AS rt
-INNER JOIN henkilonerityisosaaminen AS he ON he.HENKILO_ID = rt.HENKILO_ID
-INNER JOIN erityisosaaminen AS eo ON eo.ERITYISOSAAMINEN_ID = he.ERITYISOSAAMINEN_ID"; //hakulausekkeen alku
+$query = "SELECT * FROM ravitsemusterapeutti r
+JOIN henkilonerityisosaaminen h ON h.HENKILO_ID = r.HENKILO_ID
+JOIN erityisosaaminen e ON e.ERITYISOSAAMINEN_ID = h.ERITYISOSAAMINEN_ID"; //hakulausekkeen alku
 $i = 0; //laskuri
+$c = 0; //toinen laskuri
 
 if(isset($_GET['nimi'])){
 	$qnimi = $_GET['nimi'];
 	if($qnimi != ""){
 		if($i==0){
-			$query = $query . " WHERE " . "(rt.KOKONIMI LIKE '%" . $qnimi . "%')"; //OR (rt.SUKUNIMI LIKE '%" . $qnimi . "%')";
+			$query = $query . " WHERE " . "(r.KOKONIMI LIKE '%" . $qnimi . "%')"; //OR (rt.SUKUNIMI LIKE '%" . $qnimi . "%')";
 			$i++;
 		}
 		else{
-			$query = $query . " AND " . "(rt.KOKONIMI LIKE '%" . $qnimi . "%')"; //OR (rt.SUKUNIMI LIKE '%" . $qnimi . "%')";
+			$query = $query . " AND " . "(r.KOKONIMI LIKE '%" . $qnimi . "%')"; //OR (rt.SUKUNIMI LIKE '%" . $qnimi . "%')";
 		}
 	}
 }
@@ -35,36 +34,48 @@ if(isset($_GET['paikkakunta'])){
 	$qpaikkakunta = $_GET['paikkakunta'];
 	if($qpaikkakunta != "Paikkakunta" && $qpaikkakunta != "Etayhteys"){
 		if($i==0){
-			$query = $query . " WHERE " . "(rt.PAIKKAKUNTA='" . $qpaikkakunta . "')";
+			$query = $query . " WHERE " . "(r.PAIKKAKUNTA='" . $qpaikkakunta . "')";
 			$i++;
 		}
 		else{
-			$query = $query . " AND " . "(rt.PAIKKAKUNTA='" . $qpaikkakunta . "')";
+			$query = $query . " AND " . "(r.PAIKKAKUNTA='" . $qpaikkakunta . "')";
 		}
 	}
 	if($qpaikkakunta == "Etayhteys"){
 		if($i==0){
-			$query = $query . " WHERE " . "(rt.ETAYHTEYS=1)";
+			$query = $query . " WHERE " . "(r.ETAYHTEYS=1)";
 			$i++;
 		}
 		else{
-			$query = $query . " AND " . "(rt.ETAYHTEYS=1)";
+			$query = $query . " AND " . "(r.ETAYHTEYS=1)";
 		}
 	}
 }
 if(isset($_GET['cbox'])){
 	$qcbox = $_GET['cbox'];
 	foreach($qcbox as $var){
-		if($i==0){
-			$query = $query . " WHERE " . "(eo.OSAAMISEN_NIMI='" . $var . "')";
-			$i++;
-			//echo $var;
+		if($c==0){
+			if($i==0){
+				$query = $query . " WHERE e.OSAAMISEN_NIMI IN ('" . $var . "'";
+				$c++;
+				$i++;
+				//echo $var;
+			}
+			else{
+				$query = $query . " AND e.OSAAMISEN_NIMI IN ('" . $var . "'";
+				$c++;
+				//echo $var;
+			}
 		}
 		else{
-			$query = $query . " AND " . "(eo.OSAAMISEN_NIMI='" . $var . "')";
-			//echo $var;
+			$query = $query . ", '" . $var . "'";
+			$c++;
 		}
 	}
+	$query = $query . ") GROUP BY r.KOKONIMI HAVING COUNT(*) = " . $c;
+}
+else{
+	$query = $query . " GROUP BY r.KOKONIMI";
 }
 
 $query = $query . ";"; //hakulausekkeen loppu
@@ -77,8 +88,40 @@ $rawresults = mysqli_query($conn, $query);
 
 if(mysqli_num_rows($rawresults) > 0){ //jos tuloksia on
 	while($results = mysqli_fetch_array($rawresults)){ //looppaa kun tietojen siirto tauluun onnistuu
-		echo $results['KOKONIMI']."<br>".$results['KOULUTUS']."<br>".$results['PAIKKAKUNTA']."<br>".$results['KUVAUS']
-		."<br>".$results['EMAIL']."<br>".$results['PUHELINNRO']."<br><br>";
+		echo $results['KOKONIMI']."<br>".$results['KOULUTUS']."<br>".$results['PAIKKAKUNTA'];
+		
+		if ($results['ETAYHTEYS'] == 1){
+			echo ", etäyhteys";
+		}
+		
+		echo "<br>"."Kuvaus: ".$results['KUVAUS']."<br>"."Sähköposti: ".$results['EMAIL'].
+		"<br>"."Puhelinnumero: ".$results['PUHELINNRO']."<br>";
+		
+			//koko seuraava osio on ainoastaan olemassa sitä varten että saadaan erityisosaamiset esille
+		
+			$equery = "SELECT e.OSAAMISEN_NIMI FROM ravitsemusterapeutti r
+			JOIN henkilonerityisosaaminen h ON h.HENKILO_ID = r.HENKILO_ID
+			JOIN erityisosaaminen e ON e.ERITYISOSAAMINEN_ID = h.ERITYISOSAAMINEN_ID
+			WHERE r.KOKONIMI='" . $results['KOKONIMI'] . "';";
+			
+			$erawresults = mysqli_query($conn, $equery);
+			
+			$e = 0; //kolmas laskuri
+			
+			while($eresults = mysqli_fetch_array($erawresults)){
+				if($eresults['OSAAMISEN_NIMI'] != "Ruoanvalinta helpommaksi" AND
+				$eresults['OSAAMISEN_NIMI'] != "Energisempi arki"){
+					if($e==0){
+						echo "Erityisosaaminen: ".$eresults['OSAAMISEN_NIMI'];
+						$e++;
+					}
+					else{
+						echo ", ".$eresults['OSAAMISEN_NIMI'];
+					}
+				}
+			}
+			
+			echo "<br><br>";
 	}
 }
 else{ //jos tuloksia ei ole
@@ -86,6 +129,9 @@ else{ //jos tuloksia ei ole
 }
 
 ?>
+
+<button type="button" class="btn" style="background-color:#C1727B"><a href="haku.php">UUSI HAKU</button><br><br>
+
 </main>
 
 <?php
