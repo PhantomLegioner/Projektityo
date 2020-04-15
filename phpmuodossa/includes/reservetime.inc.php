bfc3f47a9810fc2e5233749ca1b5b6e8f853d3de
PhantomLegioner <62619661+PhantomLegioner@users.noreply.github.com>
2020-04-15 09:41:56 +0300

    Paranneltu

--- phpmuodossa/includes/reservetime.inc.php
+++ phpmuodossa/includes/reservetime.inc.php
@@ -17,74 +17,56 @@ if (isset($_POST['varaa-laheta'])) {
   //kenttien tarkistus -- KOrjaa osoite
   if (empty($varaaja_etunimi) || empty($varaaja_sukunimi) || empty($puhelinnumero) || empty($sahkoposti) || empty($terapeutti_etunimi) || empty($terapeutti_sukunimi) || empty($palvelu) || empty($paivamaara) || empty($aloitusaika) || empty($lopetusaika)) {
     header("Location: ../signup.php?error=emptyfields&etunimi=".$etunimi."&sukunimi=".$sukunimi."&paikkakunta=".$paikkakunta."&osoite=".$puhelinnumero."&sposti=".$sahkoposti);
+    echo "Sinulta puuttui tietoja. Täydennä tarvittavat tiedot.";
     exit();
 
   }
   elseif (!filter_var($sahkoposti, FILTER_VALIDATE_EMAIL)) {
     header("Location: ../signup.php?error=invalidmail&etunimi=".$etunimi."&sukunimi=".$sukunimi."&paikkakunta=".$paikkakunta."&osoite=".$puhelinnumero);
+    echo "Sähköposti oli kirjoitettu väärin.";
     exit();
   }
 
   elseif (!preg_match("/^[a-zA-Z]*$/",$etunimi)) {
     header("Location: ../signup.php?error=invalidnimi&sukunimi=".$sukunimi."&paikkakunta=".$paikkakunta."&osoite=".$puhelinnumero."&sposti=".$sahkoposti);
+    echo "Etunimessä oli vääriä merkkejä.";
     exit();
   }
 
   elseif (!preg_match("/^[a-zA-Z]*$/",$sukunimi)) {
     header("Location: ../signup.php?error=invalidnimi&etunimi=".$etunimi."&paikkakunta=".$paikkakunta."&osoite=".$puhelinnumero."&sposti=".$sahkoposti);
+    echo "Sukunimessä oli vääriä merkkejä.";
     exit();
   }
-  elseif (!preg_match("/^[a-zA-Z]*$/",$paikkakunta)) {
-    header("Location: ../signup.php?error=invalidnimi&etunimi=".$etunimi."&sukunimi=".$sukunimi."&osoite=".$puhelinnumero."&sposti=".$sahkoposti);
-    exit();
-  }
+
 
   elseif (!preg_match("/^[0-9]*$/",$puhelinnumero)) {
     header("Location: ../signup.php?error=invalidnimi&etunimi=".$etunimi."&sukunimi=".$sukunimi."&paikkakunta=".$paikkakunta."&osoite=".$puhelinnumero."&sposti=".$sahkoposti);
-    exit();
-  }
-
-  elseif ($salasana !== $salasanaToisto) {
-    header("Location: ../signup.php?error=passwordcheck&etunimi=".$etunimi."&sukunimi=".$sukunimi."&paikkakunta=".$paikkakunta."&osoite=".$puhelinnumero."&sposti=".$sahkoposti);
+    echo "Sukunimessä oli vääriä merkkejä.";
     exit();
   }
 
   //Terapeutin tarkistus
   else {
-    $sql = "SELECT ETUNIMI AND SUKUNIMI FROM ravitsemusterapeutti WHERE ETUNIMI=? AND SUKUNIMI=?";
+    $sql = "INSERT INTO varattu_aika (varaaja_etunimi, varaaja_sukunimi, puhelinnumero, email, terapeutti_etunimi, terapeutti_sukunimi, palvelu_valittu, paivamaara, aloitusaika, lopetusaika) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
     $stmt = mysqli_stmt_init($conn);
     if (!mysqli_stmt_prepare($stmt, $sql)) {
-      header("Location: ../signup.php?error=sqlerror1");
+      header("Location: ../resdervetime.inc.php?error=sqlerror2");
+      echo "Tapahtui virhe";
       exit();
     }
     else {
-      mysqli_stmt_bind_param($stmt, "s", $sahkoposti);
+      mysqli_stmt_bind_param($stmt, "sssssssdss", $varaaja_etunimi, $varaaja_sukunimi, $puhelinnumero, $sahkoposti, $terapeutti_etunimi, $terapeutti_sukunimi, $palvelu, $paivamaara, $aloitusaika, $lopetusaika);
       mysqli_stmt_execute($stmt);
-      mysqli_stmt_store_result($stmt);
-      $tarkistus = mysqli_stmt_num_rows($stmt);
-      if($tarkistus > 0) {
-        header("Location: ../signup.php?error=emailtaken");
-      }
-      else {
-        $sql = "INSERT INTO ravitsemusterapeutti (ETUNIMI, SUKUNIMI, PAIKKAKUNTA, OSOITE, EMAIL, PASSWORD, PUHELINNRO) VALUES (?, ?, ?, ?, ?, ?, ?)";
-        $stmt = mysqli_stmt_init($conn);
-        if (!mysqli_stmt_prepare($stmt, $sql)) {
-          header("Location: ../signup.php?error=sqlerror2");
-          exit();
-        }
-        else {
-          $hashedSalasana = password_hash($salasana, PASSWORD_DEFAULT); //Salasanan hashaaminen
-          mysqli_stmt_bind_param($stmt, "sssssss", $etunimi, $sukunimi, $paikkakunta, $osoite, $sahkoposti, $hashedSalasana, $puhelinnumero);
-          mysqli_stmt_execute($stmt);
-          header("Location: ../signup.php?signup=success");
-          exit();
-        }
-      }
+      header("Location: ../signup.php?signup=success");
+      echo "Ajanvaraus onnistui!";
+      exit();
     }
+  }
+
     //mysqli_stmt_close($stmt);
     //mysqli_close($conn);
 
-  }
   //else {
     //header("Location: ../signup.php");
     //exit();
